e49f2b7942533922f5359c4166ec28c2759778e3
Aline Soares da Silva <aline.as385@gmail.com>
2021-07-02 09:42:45 -0300

    testando cadastro de leiloes

--- src/test/java/leiloes/LeiloesPage.java
+++ src/test/java/leiloes/LeiloesPage.java
@@ -1,7 +1,8 @@
 package leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 
@@ -20,4 +21,15 @@ public class LeiloesPage {
         this.browser.navigate().to(URL_CADASTRO_LEILAO);
         return new CadastroLeilaoPage(browser);
 }
+
+    public boolean isLeilaoCadastrado(String nome, String valor, String data) {
+        WebElement linhaDaTabela = this.browser.findElement(By.cssSelector("#table-leiloes tbody tr:last-child")); //cssSelector usa os dados do css
+        WebElement colunaNome = linhaDaTabela.findElement(By.cssSelector("td:nth-child(1)"));
+        WebElement colunaDataAbertura = linhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
+        WebElement colunaValorInicial = linhaDaTabela.findElement(By.cssSelector("td:nth-child(3)"));
+
+        return colunaNome.getText().equals(nome)
+                && colunaDataAbertura.getText().equals(data) 
+                && colunaValorInicial.getText().equals(valor);
+    }
 }
